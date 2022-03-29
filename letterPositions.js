f157aea4eb2c68ffe8d459767135dc5ba2a436e3
Carmen Tsang <carmtsang89@hotmail.com>
2022-03-29 23:39:50 +0000

    setting up function

---
+++ letterPositions.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  const passed = `\uD83D\uDE00\uD83D\uDE00\uD83D\uDE00 Assertion Passed: ${actual} === ${expected}`;
+  const failed = `\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31 Assertion Failed: ${actual} !== ${expected}`;
+  if (actual === expected) {
+    console.log(passed);
+  } else {
+    console.log(failed);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+// function that will return all the indices in the string
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+  return results;
+}
+
+//test cases
+
+letterPositions('lighthouse in the house')
