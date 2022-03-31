836ea12842d62511cb798c6f95f430d2b6ad190a
Carmen Tsang <carmtsang89@hotmail.com>
2022-03-31 04:18:42 +0000

    lint test performed

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -15,18 +15,15 @@ const assertEqual = function(actual, expected) {
 const findKeyByValue = function(objList, searchValue) {
   const keys = Object.keys(objList); //turn object keys into an array
   for (const key of keys) {
-    if (objList[key] === searchValue) { 
-      return key
+    if (objList[key] === searchValue) {
+      return key;
     }
-
   }
-  
-  
 };
 
 //test case
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
+const bestTVShowsByGenre = {
+  "sci_fi": "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
 };
@@ -37,11 +34,11 @@ assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 
 const bestMoviesByGenre = {
-  sci_fi: "Star Trek",
+  "sci_fi": "Star Trek",
   comedy: "Hard and Kumar",
   drama: "Parasite",
   romcom: "The Holiday"
 };
 
 assertEqual(findKeyByValue(bestMoviesByGenre, "Parasite"), "drama");
-assertEqual(findKeyByValue(bestMoviesByGenre, "ShaolinSoccer"), undefined);
+assertEqual(findKeyByValue(bestMoviesByGenre, "Parasite"), undefined);
