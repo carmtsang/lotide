e1a2e6d9ed804c401913c8301050f271832b8f12
Carmen Tsang <carmtsang89@hotmail.com>
2022-03-31 19:11:47 +0000

    added in test cases for eqObjects function

--- eqObjects.js
+++ eqObjects.js
@@ -16,3 +16,15 @@ const assertEqual = function(actual, expected) {
 const eqObjects = function(object1, object2) {
 
 };
+
+// test code
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+eqObjects(ab, ba); // true
+
+assertEqual(eqObjects(ab, ba), true);
+
+const abc = { a: "1", b: "2", c: "3" };
+eqObjects(ab, abc); // false
+
+assertEqual(eqObjects(ab, abc), false);
