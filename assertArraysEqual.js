3f83afbba28c339a45e3461ad5a85496f8be68dc
Carmen Tsang <carmtsang89@hotmail.com>
2022-04-01 02:11:53 +0000

    updated the function to have eqArrays pass through assertArraysEqual. I did not understand why eqArray was needed before!

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,27 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const assertArraysEqual = function(actual, expected) {
   const passed = `😁😁😁 Assertion Passed: ${actual} === ${expected}`;
   const failed = `😱😱😱 Assertion Failed: ${actual} !== ${expected}`;
-  if (actual.length !== expected.length) {
-    console.log(failed);
-    for (let i = 0; i < actual.length; i++) {
-      if (actual[i] !== expected[i]) {
-        console.log(failed);
-      }
-    }
-  } else {
+  if (eqArrays(actual, expected)) {
     console.log(passed);
+  } else {
+    console.log(failed);
   }
 };
+
+
+
 
 //test code
 assertArraysEqual(["this", 2, 3], ["this", 2, 3]);
