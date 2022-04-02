050a6522d3abdbb601db67aaa12f8aabc189db6e
Carmen Tsang <carmtsang89@hotmail.com>
2022-04-02 21:12:50 +0000

    assetion test completed and passed

--- findKey.js
+++ findKey.js
@@ -13,18 +13,27 @@ const assertEqual = function(actual, expected) {
 const findKey = (object, callback) => {
   for (const key in object) { //loop through the object
     if (callback(object[key])) { // if the object's key = callback function. return the key
-      return key
+      return key;
     }
   }
 };
 
-let result = findKey({
+//testcase
+findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) 
+}, x => x.stars === 2);
 
-console.log(result)
+const test1 = findKey({
+  "sci_fi": "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  thriller: "The Walking Dead",
+  romcom: "Emily in Paris",
+  "post_apocalyptic": "The Walking Dead"
+}, x => x === "The Walking Dead");
+assertEqual(test1, 'thriller');
