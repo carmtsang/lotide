d10e0f8ea98e57b08d275810a4a01b1732bf5840
Carmen Tsang <carmtsang89@hotmail.com>
2022-04-02 16:27:29 +0000

    testing out for loop and callback function within map to see what is logged

--- map.js
+++ map.js
@@ -3,10 +3,13 @@
 const words = ["ground", "control", "to", "major", "tom"];
 
 const map = (array, callback) => {
-  console.log(`array: ${array}`)
-  console.log(`callback: ${callback}`)
-
   const results = [];
+  for (let item of array) {
+    console.log(`item Before: ${item}`);
+    console.log(`item AFTER: ${callback(item)}`);
+    console.log('---')
+  }
+
   return results;
 };
 
