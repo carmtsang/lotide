e2081fd620d637330e9835e6d2f4aca323359aac
Carmen Tsang <carmtsang89@hotmail.com>
2022-04-13 22:07:37 +0000

    refractor eqArraysTest to use mocha & chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,16 @@
+//const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays(['Hello', 'Lighthouse', 'Labs'],['Hello', 'Lighthouse', 'Labs']), true);
+// assertEqual(eqArrays(['Goodbye', 'Lighthouse', 'Labs'],['Hello', 'Lighthouse', 'Labs']), false);
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(['Hello', 'Lighthouse', 'Labs'],['Hello', 'Lighthouse', 'Labs']), true);
-assertEqual(eqArrays(['Goodbye', 'Lighthouse', 'Labs'],['Hello', 'Lighthouse', 'Labs']), false);
+describe('#eqArrays', () => {
+  it('should return true if both arrays input arrays are [1, 2, 3]', () => {
+    assert.strictEqual(eqArrays([1, 2, 3],[1, 2, 3]), true);
+  });
+  it('should return false if arr1 is [1, 2, 3] and arr2 is [1, "2", 3]', () => {
+    assert.strictEqual(eqArrays([1, 2, 3],[1, '2', 3]), false);
+  });
+})
