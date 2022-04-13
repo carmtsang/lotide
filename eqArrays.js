e06353c07ef83f6d92d0dc9ead8b09436caa0ac4
Carmen Tsang <carmtsang89@hotmail.com>
2022-04-13 04:12:42 +0000

    refractor eqArrays.js and created test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-  const passed = `😁😁😁 Assertion Passed: ${actual} === ${expected}`;
-  const failed = `😱😱😱 Assertion Failed: ${actual} !== ${expected}`;
-  if (actual === expected) {
-    console.log(passed);
-  } else {
-    console.log(failed);
-  }
-};
+const assertEqual = require('./assertEqual')
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
@@ -20,5 +12,4 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-//test case
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
