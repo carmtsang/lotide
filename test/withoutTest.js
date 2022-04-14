d4757b4e7d77005db27664dc878af14a765f80cf
Carmen Tsang <carmtsang89@hotmail.com>
2022-04-14 01:38:16 +0000

    refractor without.js to use mocha & chai

---
+++ test/withoutTest.js
@@ -0,0 +1,21 @@
+const assert = require('chai').assert;
+const without = require('../without');
+
+// without([1, 2, 3], [1]); // => [2, 3]
+// without(["1", "2", "3"], [1, 2, "3"]);// => ["1", "2"]
+
+// const words = ["hello", "world", "lighthouse"];
+// without(words, ["lighthouse"]); 
+// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+describe('#without', () => {
+  it('should have an arr1 [1, 2, 3], and arr2 shows what elements to remove [1], returning [2, 3]', () => {
+    assert.deepEqual(without([1, 2, 3], [1]), [2, 3])
+  });
+  it('should return an array removing elements of the same datatype', () => {
+    assert.deepEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"])
+  });
+  it('should return an emty array is both arrays are the same', () => {
+    assert.deepEqual(without(["hello", "world", "lighthouse"],["hello", "world", "lighthouse"]),[]);
+  });
+});
--- without.js
+++ without.js
@@ -1,26 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  const passed = `😁😁😁 Assertion Passed: ${actual} === ${expected}`;
-  const failed = `😱😱😱 Assertion Failed: ${actual} !== ${expected}`;
-  if (eqArrays(actual, expected)) {
-    console.log(passed);
-  } else {
-    console.log(failed);
-  }
-};
-
-
 //filter out array and remove unwanted items
 const without = function(source, itemsToRemove) {
   const newArray = [];
@@ -33,13 +10,6 @@ const without = function(source, itemsToRemove) {
 };
 
 //test scenario
-without([1, 2, 3], [1]); // => [2, 3]
-without(["1", "2", "3"], [1, 2, "3"]);// => ["1", "2"]
 
-
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
 module.exports = without;
